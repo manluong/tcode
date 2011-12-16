1678f6e6ab3d70663d2f702f1cf896b4950502db
Erik Yeoh <erik@telcoson.com>
2011-12-16 17:50:07 +0800

    Clean up Langmodel

--- application/models/langmodel.php
+++ application/models/langmodel.php
@@ -1,29 +1,34 @@
 <?php if (!defined('BASEPATH')) exit('No direct access allowed.');
 
 class Langmodel extends CI_Model {
-
 	function __construct() {
 		parent::__construct();
 	}
-	
-	function loadarray($app,$lang_use){
 
+	function loadarray($app, $lang_use){
 		//Lang file storage
 		//Get array from DB now, can change to get from file in future.
-    	$sql = 'SELECT * FROM lang_ci WHERE lang_array_app = "'.$app.'" AND lang_array_lang = "'.$lang_use.'"';
-    	$result = $this->db->query($sql);
-		$this_array = $result->row_array(0);
-		$this_array = unserialize($this_array['lang_array_array']);
-    	return $this_array;
-	}
-	
-	function initialise(){
-		
-		$sql = 'SELECT lang_use_code,lang_use_name,lang_use_default FROM lang_use WHERE lang_use_active = "1"';
-		$result = $this->db->query($sql);
-		return $result->result_array();
 
+		$rs = $this->db->select()
+				->where('lang_array_app', $app)
+				->where('lang_array_lang', $lang_use)
+				->get('lang_ci', 1);
+
+		if ($rs->num_rows() == 0) return array();
+
+    	$result = $rs->row_array();
+    	return unserialize($result['lang_array_array']);
 	}
-	
+
+	function initialise(){
+		$rs = $this->db->select('lang_use_code, lang_use_name, lang_use_default')
+				->where('lang_use_active', 1)
+				->get('lang_use');
+
+		if ($rs->num_rows() == 0) return array();
+
+		return $rs->result_array();
+	}
+
 
 }
