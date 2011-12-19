6d2499756fada636fd50237e5705a15e2d3bfd47
Erik Yeoh <erik@telcoson.com>
2011-12-19 13:15:41 +0800

    Code cleanup

--- application/core/MY_Lang.php
+++ application/core/MY_Lang.php
@@ -3,33 +3,33 @@
 class MY_Lang extends CI_Lang {
 	var $lang_use = "";
 	var $langinfo = array();
-	
+
 	function __construct() {
 		parent::__construct();
-		
+
 	}
 
 	function line($line)
-	{	
+	{
 		if (isset($this->language[$line])) return $this->language[$line];
 	}
-	
-	function loadarray($this_array){	
+
+	function loadarray($this_array){
 		$this->language = array_merge($this->language, $this_array);
 	}
-	
-	function initialise($langsetting, $app = ""){
-		
+
+	function initialise($langsetting, $app = ''){
+
 		foreach ($langsetting as $field) {
 		    $this->langinfo['langlist'][$field['lang_use_code']] = $field['lang_use_name'];
 		    if ($field['lang_use_default']) {
 		        $this->langinfo['use'] = $this->langinfo['default'] = $this->lang_use = $field['lang_use_code'];
 		    }
 		}
-		
+
 		//if there is a request send by _GET or _POST with specified LANG, change the ['use'] to the request LANG
 		//if ($CGET['thislang']) { $langinfo['thislang'] = $CGET['thislang']; $langinfo['thislang_get'] = 1; } else { $langinfo['thislang'] = $langinfo['use']; }
-		
+
 	}
 
 	function f_layout_listlang($this_list,$app){
