78bd4cb7712c39931b0df026c95a88bda37dfd60
Erik Yeoh <erik@telcoson.com>
2012-01-06 18:18:21 +0800

    UnitTest controller - added extra tests, show triggered rules

--- application/controllers/unittest.php
+++ application/controllers/unittest.php
@@ -24,11 +24,16 @@ class Unittest extends MY_Controller {
 		$tests = array(
 			1 => array(
 				'admin_client_search_1' => 'no',
-				'write_client_search_12' => 'no',
-				'read_client_search_12' => 'yes',
-				'admin_client_search_88' => 'no',
 				'admin_client_list_12' => 'no',
+				'admin_client_search_88' => 'no',
 				'admin_client_list_88' => 'yes',
+
+				'write_client_search_12' => 'no',
+
+				'read_client_search_12' => 'yes',
+
+				'move_client_list_12' => 'no',
+				'move_client_list_88' => 'yes',
 			)
 		);
 
@@ -38,14 +43,17 @@ class Unittest extends MY_Controller {
 		if ($sett_randomize_tests) $test_set = shuffle_assoc($test_set);
 
 		echo '<table border="1" cellpadding="5">';
-		echo '<tr><th>Action</th><th>App</th><th>ActionGroup</th><th>Data ID</th><th>Expected Result</th><th>Result</th>';
+		echo '<tr><th>Action</th><th>App</th><th>ActionGroup</th><th>Data ID</th><th>Expected Result</th><th>Result</th><th>Triggered Rules</th></tr>';
 		foreach($test_set AS $t => $answer) {
 			$params = explode('_', $t);
 			$test_result = ($this->ACLM->check($params[0], $params[1], $params[2], $params[3])) ? 'yes' : 'no';
+			$triggered_rule = $this->ACLM->unit_test['triggered_rule'];
 
 			echo ($test_result == $answer) ? '<tr style="background-color:#CFC;">' : '<tr style="background-color:#FCC;">';
 			echo '<td>',$params[0],'</td><td>',$params[1],'</td><td>',$params[2],'</td><td>',$params[3];
-			echo '</td><td>',$answer,'</td><td>',$test_result,'</td></tr>';
+			echo '</td><td>',$answer,'</td><td>',$test_result,'</td>';
+			echo '<td>',implode(', ', $triggered_rule),'</td>';
+			echo '</tr>';
 		}
 
 
