b333100c3304daf8fe10d91743ac6fdd258b1dfa
Erik Yeoh <erik@telcoson.com>
2012-01-17 17:14:48 +0800

    TenantM->get_db_connection($tenant_id)
    Returns a DB object which you can use to insert data into the tenant's
    database

--- application/models/tenantm.php
+++ application/models/tenantm.php
@@ -10,7 +10,21 @@ class TenantM extends MY_Model {
 	}
 
 
+	function get_db_connection($tenant_id) {
+		$tenant = $this->get($tenant_id);
 
+		$db_config = array(
+			'hostname' => $tenant['dbhost'],
+			'username' => $tenant['dbuser'],
+			'password' => $tenant['dbpass'],
+			'database' => $tenant['dbname'],
+			'dbdriver' => 'mysql',
+			'char_set' => 'utf8',
+			'dbcollat' => 'utf8_general_ci'
+		);
+
+		return $this->load->database($db_config, TRUE);
+	}
 
 }
 ?>
