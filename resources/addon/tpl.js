b5637aafe95e11c557bfc57e0a30e524ba78d0f1
Edward Au <edward_au@Edward-Aus-iMac.local>
2012-02-14 01:44:05 +0800

    update tpl.js

--- resources/addon/tpl.js
+++ resources/addon/tpl.js
@@ -6,9 +6,13 @@ tpl_form.text = '<input type="text" class="{{addclass}}" id="form_{{name}}" valu
 tpl_form.email = '<input type="email" class="{{addclass}}" id="form_{{name}}" value="{{value}}">';
 tpl_form.number = '<input type="number" class="{{addclass}}" id="form_{{name}}" value="{{value}}">';
 tpl_form.url = '<input type="url" class="{{addclass}}" id="form_{{name}}" value="{{value}}">';
-tpl_form.date = '<input type="email" class="{{addclass}}" id="form_{{name}}" value="{{value}}">';
-tpl_form.datetime = '<input type="email" class="{{addclass}}" id="form_{{name}}" value="{{value}}">';
-tpl_form['time'] = '<input type="email" class="{{addclass}}" id="form_{{name}}" value="{{value}}">';
+
+tpl_form.password = '<input type="password" class="{{addclass}}" id="form_{{name}}" value="{{value}}">';
+
+tpl_form.date = '<input type="date" class="{{addclass}}" id="form_{{name}}" value="{{value}}">';
+tpl_form.datetime = '<input type="date" class="{{addclass}}" id="form_{{name}}" value="{{value}}">';
+tpl_form['time'] = '<input type="time" class="{{addclass}}" id="form_{{name}}" value="{{value}}">';
+
 tpl_form.hidden = '<input type="email" class="{{addclass}}" id="form_{{name}}" value="{{value}}">';
 tpl_form.file = '<input type="email" class="{{addclass}}" id="form_{{name}}" value="{{value}}">';
 tpl_form.checkbox = '<input type="email" class="{{addclass}}" id="form_{{name}}" value="{{value}}">';
