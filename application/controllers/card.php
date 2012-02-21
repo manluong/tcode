5d16ada0f766da073afea94e02218fe9416ea612
Erik Yeoh <erik@telcoson.com>
2012-02-21 19:01:28 +0800

    Card app list view

--- application/controllers/card.php
+++ application/controllers/card.php
@@ -9,5 +9,25 @@ class Card extends MY_Controller {
 		$this->model =& $this->UserM;
 	}
 
+	function x_card() {
+		$this->DatasetM->load('info');
+
+		$details = array(
+			'columns' => $this->DatasetM->get_datatable_fields(),
+			'data' => $this->DatasetM->get_datatable_data(),
+			'ids' => $this->DatasetM->get_list_ids(),
+			'setting' => array(
+				'hidetitle' => 0,
+			),
+		);
+
+		$this->RespM->set_message($this->DatasetM->sql)
+				->set_type('list')
+				->set_template('')
+				->set_success(true)
+				->set_title('Contacts List')
+				->set_details($details)
+				->output_json();
+	}
 
 }
