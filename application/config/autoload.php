1e7319e01df4425ac1a3c6b245da5a5ae2cee8a7
Erik Yeoh <erik@telcoson.com>
2012-03-02 16:39:13 +0800

    Do no autoload session and database library

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('session','database','encrypt','lang');
+$autoload['libraries'] = array('encrypt','lang');
 
 
 /*
