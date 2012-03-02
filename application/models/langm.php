72c42b6007825c0d4105442d69b1fe51feb61de4
Erik Yeoh <erik@telcoson.com>
2012-03-02 17:12:44 +0800

    LangM - to use global_setting DB

--- application/models/langm.php
+++ application/models/langm.php
@@ -12,7 +12,7 @@ class LangM extends CI_Model {
 	function load_active_languages(){
 		$rs = $this->db->select('lang_use_code, lang_use_name, lang_use_default')
 				->where('lang_use_active', 1)
-				->get('lang_use');
+				->get('global_setting.lang_use');
 
 		if ($rs->num_rows() == 0) return FALSE;
 
@@ -26,7 +26,7 @@ class LangM extends CI_Model {
 		$rs = $this->db->select('lang_array_array')
 				->where('lang_array_app', $app)
 				->where('lang_array_lang', $language)
-				->get('lang_ci', 1);
+				->get('global_setting.lang_ci', 1);
 
 		if ($rs->num_rows() == 0) return array();
 
