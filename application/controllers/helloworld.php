51712d31536b2f8762cb2f1eadf172cf06449190
Erik Yeoh <erik@telcoson.com>
2012-04-16 13:20:54 +0800

    Helloworld fix

--- application/controllers/helloworld.php
+++ application/controllers/helloworld.php
@@ -8,6 +8,17 @@ class Helloworld extends MY_Controller {
 		$this->load->model('DS_HelloWorld');
 	}
 
+	function index() {
+		$data = array();
+		$data['html'] = $this->load->view(get_template().'/helloworld/index', '', TRUE);
+
+		$this->data[] = $data;
+
+		$this->LayoutM->load_format();
+
+		$this->output();
+	}
+
 	function sendhtml() {
 		$data = array();
 		$data['html'] = "I am a some HTML";
---
+++ application/views/default_web/helloworld/index.php
@@ -0,0 +1,16 @@
+<div id="helloworld_list">
+</div>
+
+<div id="helloworld_view">
+</div>
+
+<div id="helloworld_form">
+</div>
+
+<script>
+	$(document).ready(function() {
+		ajax_content('/helloworld/sendjson_list', 'helloworld_list');
+		ajax_content('/helloworld/sendjson_view', 'helloworld_view');
+		ajax_content('/helloworld/sendjson_form', 'helloworld_form');
+	});
+</script>
