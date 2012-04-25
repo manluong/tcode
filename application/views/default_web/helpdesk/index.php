e5afe76cb819d96790c9535b8ffd40ff05dfd557
leo9x <tosedo@gmail.com>
2012-04-25 09:08:15 +0700

    fix

--- application/views/default_web/helpdesk/index.php
+++ application/views/default_web/helpdesk/index.php
@@ -9,8 +9,8 @@
 
 <script>
 	$(document).ready(function() {
-		ajax_content('http://apple.local.net/tcode/helpdesk/sendjson_list', 'helloworld_list');
-		ajax_content('http://apple.local.net/tcode/helpdesk/sendjson_view', 'helloworld_view');
-		ajax_content('http://apple.local.net/tcode/helpdesk/sendjson_form', 'helloworld_form');
+		ajax_content('/helpdesk/sendjson_list', 'helloworld_list');
+		ajax_content('/helpdesk/sendjson_view', 'helloworld_view');
+		ajax_content('/tcode/helpdesk/sendjson_form', 'helloworld_form');
 	});
 </script>
