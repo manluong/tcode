d2b76877dfefde5bb9b36695638af7f123178386
Man Luong <luongtheman87@gmail.com>
2012-05-05 12:16:33 +0700

    helpdesk comment

---
+++ application/views/default_web/helpdesk/ajax_updateComment.php
@@ -0,0 +1,13 @@
+<?php if(!empty($comment)){
+		foreach($comment as $k){
+		$date = date('F d Y g:iA',strtotime($k->stamp));
+?>
+<div id="comment_content_left">
+	<div id="wap_comment_left">
+		<div id="wap_icon_mail"><image src="/resources/template/<?=get_template()?>/img/mail.png" /></div>
+		<div id="comment_name">Andy<br/><a href="#">Company A</a></div>
+		<div id="comment_time"><?=$date?></div>
+	</div>
+	<div id="comment_content"><?=$k->comment?></div>
+</div>
+<?php }}?>
