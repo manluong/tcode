0b8ba95757ef6de426b174a2016f651a1e45f56c
Erik Yeoh <erik@telcoson.com>
2012-05-11 17:45:53 +0800

    Remove plupload and flexpaper from <head>, load them at the app's view

--- application/views/default_web/docs/docs_js.php
+++ application/views/default_web/docs/docs_js.php
@@ -1,3 +1,8 @@
+<script type="text/javascript" src="http://bp.yahooapis.com/2.4.21/browserplus-min.js"></script>
+<script type="text/javascript" src="/resources/addon/plupload/js/plupload.full.js"></script>
+<script type="text/javascript" src="/resources/addon/plupload/js/jquery.plupload.queue/jquery.plupload.queue.js"></script>
+<link href="/resources/addon/plupload/js/jquery.plupload.queue/css/jquery.plupload.queue.css" media="screen" rel="stylesheet" type="text/css" />
+
 <script>
 	var directory_contents = '';
 
