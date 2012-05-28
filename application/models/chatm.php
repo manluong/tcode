a3af2e3667492c54b4a7235ccb2eed30b766c730
leo9x <tosedo@gmail.com>
2012-05-28 14:07:16 +0700

    add chat

---
+++ application/models/chatm.php
@@ -0,0 +1,7 @@
+<?php if (!defined('BASEPATH')) exit('No direct access allowed.');
+// Leo
+    class ChatM extends MY_Model {
+        function __construct() {
+            parent::__construct();
+	}
+    }
