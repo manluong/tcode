0ed2b86b86c828c3c6862cefe21139b08f920d1e
leo9x <tosedo@gmail.com>
2012-05-28 13:40:25 +0700

    add chat box

--- resources/template/default_web/js/chat.js
+++ resources/template/default_web/js/chat.js
@@ -1,8 +1,10 @@
 jQuery("#show_hide_chat").click(function(){
   if(document.getElementById('list_chat').style.display == 'none'){
       jQuery("#list_chat").show();
+      jQuery("#show_hide_chat").addClass('active');
   }
   else {
       jQuery("#list_chat").hide();
+      jQuery("#show_hide_chat").removeClass('active');
   }
 });
