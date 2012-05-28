5f3bdccdae8784c263a42376f377a37018b54394
leo9x <tosedo@gmail.com>
2012-05-28 14:27:08 +0700

    chat model

--- resources/template/default_web/js/chat.js
+++ resources/template/default_web/js/chat.js
@@ -1,10 +1,21 @@
-jQuery("#show_hide_chat").click(function(){
-  if(document.getElementById('list_chat').style.display == 'none'){
-      jQuery("#list_chat").show();
-      jQuery("#show_hide_chat").addClass('active');
-  }
-  else {
-      jQuery("#list_chat").hide();
-      jQuery("#show_hide_chat").removeClass('active');
-  }
+jQuery(document).ready(function(){
+   // effect
+    jQuery("#show_hide_chat").click(function(){
+        if(document.getElementById('list_chat').style.display == 'none'){
+            jQuery("#list_chat").show();
+            jQuery("#show_hide_chat").addClass('active');
+        }
+        else {
+            jQuery("#list_chat").hide();
+            jQuery("#show_hide_chat").removeClass('active');
+        }
+    });
+   //
+   // Chat
+   
+
+   //
+
+
+
 });
