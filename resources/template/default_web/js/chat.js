32245ec34e4be0e7504b0909d64c69f4723f37cd
leo9x <tosedo@gmail.com>
2012-05-30 01:24:37 +0700

    add chat

--- resources/template/default_web/js/chat.js
+++ resources/template/default_web/js/chat.js
@@ -227,3 +227,12 @@ jQuery("#chat_status").click(function(){
 jQuery("#set_status a.mt15").click(function(){
     jQuery("#set_status").slideUp();
 });
+jQuery("#set_status .iChat1").click(function(){
+    telcoson.status('online');
+});
+jQuery("#set_status .iChat7").click(function(){
+    telcoson.status('away');
+});
+jQuery("#set_status .iChat8").click(function(){
+    telcoson.status('offline');
+});
