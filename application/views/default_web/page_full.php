102e6d96f827a1cda1cab6822afc803f01bfec10
leo9x <tosedo@gmail.com>
2012-05-29 21:53:55 +0700

    add chat

--- application/views/default_web/page_full.php
+++ application/views/default_web/page_full.php
@@ -76,6 +76,34 @@
                                 <a href="#" class="fs11 cf6 mb0 fwb">Group Chat</a>
                             </div>
                         </div>
+                        <div class="chatBoxItem fl pv5 ph10 bgN setStatus">
+                            <p class="cf5 mb0 fl fs10 mt10 mr10 fwb w40 tac">Change Status</p>
+                            <a href="#" class="fl mr5 mt3 tac pt5 ph5 tdn status">
+                                <i class="iChat iChat1"></i>
+                                <div class="clear"></div>
+                                <span class=" cf6 fwb fs10">Online</span>
+                            </a>
+                            <a href="#" class="fl mr5 mt3 tac pt5 ph5 tdn status">
+                                <i class="iChat iChat7 dpib"></i>
+                                <div class="clear"></div>
+                                <span class=" cf6 fwb fs10">Do Not Distrub</span>
+                            </a>
+                            <a href="#" class="fl mr5 mt3 tac pt5 ph5 tdn status">
+                                <i class="iChat iChat8"></i>
+                                 <div class="clear"></div>
+                                <span class=" cf6 fwb fs10">Offline</span>
+                            </a>
+                            <a href="#" class="fl mt15 fs14 fwb cf6">x</a>
+                        </div>
+                        <div class="chatBoxItem fl pv5 ph10 bgN">
+                            <div class="fl w75p">
+                                <input class="inv-field w100p " type="text" >
+                            </div>
+                            <div class="tools fr">
+                                <a href="#" class="fl mr5 mt3"><i class="iChat iChat2"></i></a>
+                                <a href="#" class="fl w18 mt7"><input type="checkbox" class="styled" /></i></a>
+                            </div>
+                        </div>
                         <!-- List chat here -->
                     </div>
                 </div>
