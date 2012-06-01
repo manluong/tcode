c1df5f24219784a1861741456a0f9a582b04de14
longnguyen <fafinalout@gmail.com>
2012-06-01 17:47:05 +0700

    invoice

---
+++ application/models/invoice_taxm.php
@@ -0,0 +1,24 @@
+<?php if (!defined('BASEPATH')) exit('No direct access allowed.');
+
+class Invoice_TaxM extends MY_Model {
+
+	public $data_fields = array(
+		'invoice_id' => array(
+			'type' => 'id'
+		),
+		'tax_id' => array(
+			'type' => 'id'
+		),
+		'amount' => array(
+			'type' => 'numeric'
+		)
+	);
+
+	function __construct() {
+		parent::__construct();
+
+		$this->table = 'a_invoice_tax';
+		$this->cache_enabled = TRUE;
+		$this->sett_has_system_fields = FALSE;
+	}
+}
