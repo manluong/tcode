557f7c6419444dd7dbd85f4e5f51ca584b72e747
Phuc Nguyen <tosedo@gmail.com>
2012-06-08 16:56:11 +0700

    add chat

--- application/models/chatm.php
+++ application/models/chatm.php
@@ -18,22 +18,22 @@
             $output = curl_exec($ch);
             echo $output;
 	}
-		function add_user($name,$company,$realname){
-			$context = stream_context_create(array(
-				'http' => array(
-					'header'  => "Authorization: Basic bGFtcHA6dGVsY29zb25AMTk=" 
-				)
-			));
-			$content = file_get_contents("http://46.137.219.16/xmpp/create.php?name=$name&realname=".base64_encode($realname)."&company=$company",false,$context);
-			return $content;
-		}
-		function delete_user($name,$company){
-			$context = stream_context_create(array(
-				'http' => array(
-					'header'  => "Authorization: Basic bGFtcHA6dGVsY29zb25AMTk=" 
-				)
-			));
-			$content = file_get_contents("http://46.137.219.16/xmpp/delete.php?name=$name&company=$company",false,$context);
-			return $content;
-		}
+        function add_user($name,$company,$realname){
+            $context = stream_context_create(array(
+                    'http' => array(
+                            'header'  => "Authorization: Basic bGFtcHA6dGVsY29zb25AMTk=" 
+                    )
+            ));
+            $content = file_get_contents("http://46.137.219.16/xmpp/create.php?name=$name&realname=".base64_encode($realname)."&company=$company",false,$context);
+            return $content;
+        }
+        function delete_user($name,$company){
+            $context = stream_context_create(array(
+                    'http' => array(
+                            'header'  => "Authorization: Basic bGFtcHA6dGVsY29zb25AMTk=" 
+                    )
+            ));
+            $content = file_get_contents("http://46.137.219.16/xmpp/delete.php?name=$name&company=$company",false,$context);
+            return $content;
+        }
     }
