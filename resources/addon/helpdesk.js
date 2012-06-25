80209073a1c71222c62537bec5c17a598160e378
Erik Yeoh <erik@telcoson.com>
2012-06-25 18:59:17 +0800

    Use relative url, start with /

--- resources/addon/helpdesk.js
+++ resources/addon/helpdesk.js
@@ -1,5 +1,3 @@
-var ROOT = 'http://apple.8force.net/tcode/';
-
 function submit_insert_helpdesk() {
 	var comment = $('#comment').val();
 	var requester = $('#customer_id').val();
@@ -14,7 +12,7 @@ function submit_insert_helpdesk() {
 	var priority = $('#priority').val();
 
 	var url = '/helpdesk/save_insert_helpdesk';
-	
+
 	//Validate data input
 	if(subject == ''){
 		alert('Please input subject !');
@@ -48,7 +46,7 @@ function submit_insert_helpdesk() {
 
 function multiEmail(email_field) {
 	var emailReg = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-	
+
 	var email = email_field.split(';');
 	for (var i = 0; i < email.length; i++) {
 		if (!emailReg.test(email[i].trim())) {
@@ -57,7 +55,7 @@ function multiEmail(email_field) {
 		}
 	}
 	return true;
-} 
+}
 
 function insert_comment() {
 	var comment = $('#comment').val();
@@ -224,7 +222,7 @@ function helpdesk_fillter_all(){
 		}
 	);
 }
-		
+
 function load_datatable(data){
 	$('#example').dataTable( {
 		"sDom": "<<'pull-right'p>>t<<'pull-right'p>lfi>",
@@ -274,7 +272,7 @@ $(document).ready(function(){
 		var id = $('#hiddenIdAdmincp').val();
 
 		var url = '/helpdesk/ajaxChangeInfoHelpDesk';
-		
+
 		//Validate data input
 		if(subject == ''){
 			alert('Please input subject !');
@@ -314,8 +312,8 @@ function submit_comment(){
 			priority : priority,
 		},function(data){
 		}
-	);	
-	
+	);
+
 	if ($('#private').is(':checked')) {
 		var pri = 1;
 	} else {
@@ -348,7 +346,7 @@ function parse_comment_list(data){
 	var json = jQuery.parseJSON(data);
 	var html ='';
 	var input_hidden = '';
-	for (i in json) {	
+	for (i in json) {
 		 var item = json[i];
 		 var display_name = (item.display_name == null ? '' : item.display_name);
 		 var organization_name = (item.organization_name == null ? '' : item.organization_name);
@@ -358,7 +356,7 @@ function parse_comment_list(data){
 			case 1:
 				pri = 'Low';
 				break;
-			case 2: 
+			case 2:
 				pri = 'Mid';
 				break;
 			case 3:
@@ -367,7 +365,7 @@ function parse_comment_list(data){
 		 }
 		 html += '<div class="comment_info">'+
 						'<div class="comment_info_user">'+
-							'<div class="comment_user_avatar"><image src="'+ROOT+'resources/template/default_web/img/helpdesk/comment_avatar.png"/></div>'+
+							'<div class="comment_user_avatar"><image src="/resources/template/default_web/img/helpdesk/comment_avatar.png"/></div>'+
 								'<div class="comment_user_name">'+
 								'<div style="color:#444444;font-size:13px;width:100%;height:14px;"><strong>'+display_name+'</strong>'+organization_name+'</div>'+
 								'<div style="font-size:11px;color:#b0b0b0;">'+prettyDate(item.created_stamp)+'</div>'+
@@ -397,10 +395,10 @@ function prettyDate(time){
 	var date = new Date((time || "").replace(/-/g,"/").replace(/[TZ]/g," ")),
 		diff = (((new Date()).getTime() - date.getTime()) / 1000),
 		day_diff = Math.floor(diff / 86400);
-			
+
 	if ( isNaN(day_diff) || day_diff < 0 || day_diff >= 31 )
 		return;
-			
+
 	return day_diff == 0 && (
 			diff < 60 && "just now" ||
 			diff < 120 && "1 minute ago" ||
