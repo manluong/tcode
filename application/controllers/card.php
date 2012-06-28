5d7c8e4697b87df29fdb0c3b165f52ba6f7fa643
Phúc Nguyễn <tosedo@gmail.com>
2012-06-28 23:24:01 +0700

    help iphone dev

--- application/controllers/card.php
+++ application/controllers/card.php
@@ -532,16 +532,17 @@ class Card extends MY_Controller {
 				$addon_email = explode(';',$addon_email);
 				for($i=0 ; $i<count($addon_email) ; $i++){
 					$email = explode(',',$addon_email[$i]);
-						$_POST['addon_email'][$i]['id'] = trim($email[0]);
-						$_POST['addon_email'][$i]['email'] = trim($email[1]);
-						$_POST['addon_email'][$i]['type'] = trim($email[2]);
-						$_POST['addon_email'][$i]['is_default'] = trim($email[3]);
+//						$_POST['addon_email'][$i]['id'] = trim($email[0]);
+//						$_POST['addon_email'][$i]['email'] = trim($email[1]);
+//						$_POST['addon_email'][$i]['type'] = trim($email[2]);
+//						$_POST['addon_email'][$i]['is_default'] = trim($email[3]);
 						$array[$i]['id'] = $email[0];
 						$array[$i]['email'] = $email[1];
 						$array[$i]['type'] = $email[2];
 						$array[$i]['is_default'] = $email[3];
 				}
-			print_r($_POST['addon_email']);
+			$_POST['addon_email'] = $array;
+			print_r($_POST);
 			//print_r($array);
 
 			}
