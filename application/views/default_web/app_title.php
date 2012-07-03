027850b24a5e94266e7d95b616d3119c64901e2d
Edward Au <ea@telcoson.com>
2012-07-03 21:10:52 +0800

    update apptitle

--- application/views/default_web/app_title.php
+++ application/views/default_web/app_title.php
@@ -1,29 +1,5 @@
 <div id="breadcrumb">
-	<div id="module_name">
-		<ul>
-			<li><a href="#" class="main"><?=$active_app_name;?></a></li>
-			<?php
-				$count_breadcrumb = count($breadcrumb);
-				if ($count_breadcrumb > 0) {
-					echo '<li class="arrow"></li>';
-					$x = 1;
-					foreach($breadcrumb AS $bc) {
-						echo '<li';
-						//if ($x == $count_breadcrumb) echo ' class="curent_page"';
-						echo ' class="simple"';
-						echo '>';
-						if (!empty($bc['url'])) echo '<a href="',$bc['url'],'">';
-						echo $bc['title'];
-						if (!empty($bc['url'])) echo '</a>';
-						echo '</li>';
-						if ($x != $count_breadcrumb) echo '<li class="arrow"></li>';
-						$x++;
-					}
-				}
-			?>
-			<li><a href="#" id="favoriteIcon" class="on" title="Remove from favorites"></a></li>
-		</ul>
-	</div>
+
 	<?php if (count($app_menu) > 0) { ?>
 	<div class="rightBar">
 		<?php foreach($app_menu AS $menu) { ?>
@@ -31,18 +7,37 @@
 		<?php } ?>
 	</div>
 	<?php } ?>
+		
+	<?php
+		//back link
+		$return_url = get_return_url();
+		if (strlen($return_url) > 0) {
+			//<a class="simple" href="#">BACK TO MAIN</a><span class="arrowBack"></span>
+			$return_text = str_replace(
+								'#app_name#',
+								$this->lang->line('core_apps-name-'.$this->re_url['app']),
+								$this->lang->line('core-return_to_app')
+							);
+			//echo '<div style="margin-bottom:20px;">';
+			//echo '<a href="/',$return_url,'" class="btn">',$return_text,'</a>';
+			//echo '</div>';
+			echo '<a class="simple" href="/',$return_url,'">',$return_text,'</a><span class="arrowBack"></span>';
+		}
+	?>
+	<a href="#" class="main"><?=$active_app_name;?></a>
+	<?php
+		$count_breadcrumb = count($breadcrumb);
+		if ($count_breadcrumb > 0) {
+			$x = 1;
+			foreach($breadcrumb AS $bc) {
+				echo '<span class="arrow"></span>';
+				if (!empty($bc['url'])) echo '<a class="simple tabsContent" href="',$bc['url'],'">';
+				echo $bc['title'];
+				if (!empty($bc['url'])) echo '</a>';
+				$x++;
+			}
+		}
+	?>
+	<a class="simple" href="#" class="star"></a> <a href="#" id="favoriteIcon" class="on" title="Remove from favorites"></a>
+	
 </div>
-
-<?php
-	$return_url = get_return_url();
-	if (strlen($return_url) > 0) {
-		$return_text = str_replace(
-							'#app_name#',
-							$this->lang->line('core_apps-name-'.$this->re_url['app']),
-							$this->lang->line('core-return_to_app')
-						);
-		echo '<div style="margin-bottom:20px;">';
-		echo '<a href="/',$return_url,'" class="btn">',$return_text,'</a>';
-		echo '</div>';
-	}
-?>
