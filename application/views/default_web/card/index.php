c116994b7184e829e9c1cea9aac96ed1342c6f68
Phúc Nguyễn <tosedo@gmail.com>
2012-07-04 14:16:12 +0700

    update contact module layout

--- application/views/default_web/card/index.php
+++ application/views/default_web/card/index.php
@@ -1,3 +1,14 @@
+<!--<link rel="stylesheet" href="/resources/template/<?=get_template()?>/css/nanoscroller.css" />-->
+<script type="text/javascript" src="/resources/addon/overthrow.min.js"></script>
+<script type="text/javascript" src="/resources/addon/contacts.js"></script>
+<script>
+$(document).ready(function(){
+	//LOAD CONTACT INFO FIRST
+	hide_empty_contact();
+	load_contact_info(1);
+	jQuery("#breadcrump").prepend('<div class="rightBar"><button class="btn btn-inverse">NEW</button></div>');
+});
+</script>
 <!-- Start Contacts' list -->
 		<div id="contactsPanel">
 			<input style="color: rgb(192, 192, 192);" id="searchContacts" onclick="value =''" onblur="(value =='' ? value='Search contacts':value=value)" value="Search contacts" type="text">
@@ -15,31 +26,31 @@
 						<li onclick="load_contact_info(228)" class="contact client a-contact">
 
 	                    <div class="contactType">Client</div>
-	                    <img src="resources/template/default_web/img/avatar.jpg" alt="" width="32" width="32">
+	                    <img src="/resources/template/default_web/img/avatar.jpg" alt="" width="32" width="32">
 	                    <div class="info hasCompany"><a href="http://apple.8force.net/card/view/228">aaa cccc</a></div>
 	                    <div class="company"></div></li><li onclick="load_contact_info(237)" class="contact client (additional) a-contact">
 
 	                    <div class="contactType">Client (additional)</div>
-	                    <img src="resources/template/default_web/img/avatar.jpg" alt="" width="32" width="32">
+	                    <img src="/resources/template/default_web/img/avatar.jpg" alt="" width="32" width="32">
 	                    <div class="info hasCompany"><a href="http://apple.8force.net/card/view/237">aaa </a></div>
 	                    <div class="company"></div></li><li onclick="load_contact_info(243)" class="contact client a-contact">
 
 	                    <div class="contactType">Client</div>
-	                    <img src="resources/template/default_web/img/avatar.jpg" alt="" width="32" width="32">
+	                    <img src="/resources/template/default_web/img/avatar.jpg" alt="" width="32" width="32">
 	                    <div class="info hasCompany"><a href="http://apple.8force.net/card/view/243">aaa aaa</a></div>
 	                    <div class="company">aaa</div></li><li onclick="load_contact_info(244)" class="contact client (additional) a-contact">
 
 	                    <div class="contactType">Client (additional)</div>
-	                    <img src="resources/template/default_web/img/avatar.jpg" alt="" width="32" width="32">
+	                    <img src="/resources/template/default_web/img/avatar.jpg" alt="" width="32" width="32">
 	                    <div class="info hasCompany"><a href="http://apple.8force.net/card/view/244">aaa aaa</a></div>
 	                    <div class="company">aaa</div></li><li onclick="load_contact_info(256)" class="contact client a-contact">
 
-	                    <div class="contactType">Client</div><img src="resources/template/default_web/img/avatar.jpg" alt="" width="32">
+	                    <div class="contactType">Client</div><img src="/resources/template/default_web/img/avatar.jpg" alt="" width="32">
 	                    <div class="info hasCompany"><a href="http://apple.8force.net/card/view/256">Andy Lau</a></div>
 	                    <div class="company">Andy Co.</div></li><li onclick="load_contact_info(254)" class="contact  a-contact active">
 	                    <div class="contactType"></div>
 
-	                    <img src="resources/template/default_web/img/avatar.jpg" alt="" width="32"><div class="info hasCompany"><a href="http://apple.8force.net/card/view/254">Andy Lau</a></div><div class="company">Andy Co.</div></li><li onclick="load_contact_info(255)" class="contact  a-contact"><div class="contactType"></div><img src="resources/template/default_web/img/avatar.jpg" alt="" width="32"><div class="info hasCompany"><a href="http://apple.8force.net/card/view/255">Andy Lau</a></div><div class="company">Andy Co.</div></li><li onclick="load_contact_info(149)" class="contact  a-contact"><div class="contactType"></div><img src="resources/template/default_web/img/avatar.jpg" alt="" width="32"><div class="info hasCompany"><a href="http://apple.8force.net/card/view/149">Andyyyy Anthonyyyyy</a></div><div class="company">ABC Co.</div></li><li style="display: none;" class="letter staff customers vendors b-title">b</li><li class="letter staff customers vendors c-title">c</li><li onclick="load_contact_info(222)" class="contact client c-contact"><div class="contactType">Client</div><img src="resources/template/default_web/img/avatar.jpg" alt="" width="32"><div class="info hasCompany"><a href="http://apple.8force.net/card/view/222">Customer One</a></div><div class="company"></div></li><li onclick="load_contact_info(224)" class="contact client c-contact"><div class="contactType">Client</div></li>				</ul>
+	                    <img src="/resources/template/default_web/img/avatar.jpg" alt="" width="32"><div class="info hasCompany"><a href="http://apple.8force.net/card/view/254">Andy Lau</a></div><div class="company">Andy Co.</div></li><li onclick="load_contact_info(255)" class="contact  a-contact"><div class="contactType"></div><img src="/resources/template/default_web/img/avatar.jpg" alt="" width="32"><div class="info hasCompany"><a href="http://apple.8force.net/card/view/255">Andy Lau</a></div><div class="company">Andy Co.</div></li><li onclick="load_contact_info(149)" class="contact  a-contact"><div class="contactType"></div><img src="/resources/template/default_web/img/avatar.jpg" alt="" width="32"><div class="info hasCompany"><a href="http://apple.8force.net/card/view/149">Andyyyy Anthonyyyyy</a></div><div class="company">ABC Co.</div></li><li style="display: none;" class="letter staff customers vendors b-title">b</li><li class="letter staff customers vendors c-title">c</li><li onclick="load_contact_info(222)" class="contact client c-contact"><div class="contactType">Client</div><img src="/resources/template/default_web/img/avatar.jpg" alt="" width="32"><div class="info hasCompany"><a href="http://apple.8force.net/card/view/222">Customer One</a></div><div class="company"></div></li><li onclick="load_contact_info(224)" class="contact client c-contact"><div class="contactType">Client</div></li>				</ul>
 
 				<div class="pane"><div style="height: 50px;" class="slider"></div></div>
 
@@ -62,7 +73,7 @@
 						</div>
 					</div>
 					<div class="quickjump_card">
-					<div class="quickjump_avatar avatar rounded20 fl mr5"><img width="40" height="40" class="rounded20" alt="" src="resources/template/default_web/img/avatar.jpg"></div>
+					<div class="quickjump_avatar avatar rounded20 fl mr5"><img width="40" height="40" class="rounded20" alt="" src="/resources/template/default_web/img/avatar.jpg"></div>
 						<div class="quickjump_info">
 							<ul>
 								<li class="quickjump_title">Mr.</li>
